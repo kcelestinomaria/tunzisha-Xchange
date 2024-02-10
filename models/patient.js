9fea40270cd1e29ca46561b2d85ed58a24c2d4d9
Celestine Kariuki <celestino@safariblocks.com>
2024-02-10 23:05:32 +0000

    firstName, secondName for doctors

--- models/patient.js
+++ models/patient.js
@@ -8,7 +8,11 @@ const Patient = sequelize.define('Patient', {
     autoIncrement: true,
     primaryKey: true,
   },
-  name: {
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  secondName: {
     type: DataTypes.STRING,
     allowNull: false,
   },
@@ -16,7 +20,13 @@ const Patient = sequelize.define('Patient', {
     type: DataTypes.INTEGER,
     allowNull: false,
   },
+  dateOfBirth: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
   // Add more fields as needed
 });
+
+Patient.hasMany(MedicalRecord, { foreignKey: 'patientId'})
 
 module.exports = Patient;
