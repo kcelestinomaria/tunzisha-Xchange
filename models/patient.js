fccc1b92f33ed7932c3868a8975672208f3ab5a1
Celestine Kariuki <celestino@safariblocks.com>
2024-02-10 19:58:13 +0000

    Patient DB Model

---
+++ models/patient.js
@@ -0,0 +1,22 @@
+// models/patient.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Patient = sequelize.define('Patient', {
+  patientId: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  age: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  // Add more fields as needed
+});
+
+module.exports = Patient;
